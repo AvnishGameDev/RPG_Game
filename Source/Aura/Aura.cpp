e8dab202cf28338c52d23fae460852da23f13dea
Avnish Kirnalli <avnishgamedev@gmail.com>
2024-05-10 12:58:03 +0530

    Created AuraCharacter class.

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright AvnishGameDev 2024.
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright AvnishGameDev 2024.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
