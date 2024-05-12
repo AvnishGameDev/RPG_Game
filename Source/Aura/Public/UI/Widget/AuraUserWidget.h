75a57beab73318f64b76166ce726533439d73985
Avnish Kirnalli <avnishgamedev@gmail.com>
2024-05-12 20:44:59 +0530

    Created AuraUserWidget class for all widgets and added Globe Progress bars for Health and Mana in WBP_Overlay

--- Source/Aura/Public/UI/Widget/AuraUserWidget.h
+++ Source/Aura/Public/UI/Widget/AuraUserWidget.h
@@ -10,5 +10,17 @@ UCLASS()
 class AURA_API UAuraUserWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void SetWidgetController(UObject* InWidgetController);
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UObject> WidgetController;
+
+protected:
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnWidgetControllerSet();
 	
 };
